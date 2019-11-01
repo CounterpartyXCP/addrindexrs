a7c17befe87b5181e8988c0d7ff4420cf45b1a71
kenshin-samourai <kenshin_samourai@tutanota.com>
2019-11-01 17:47:01 +0100

    remove unused umport

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
 use bitcoin_hashes::sha256d::Hash as Sha256dHash;
 use std::sync::{Arc, Mutex};
 
-use crate::{config::Config, daemon, errors::*, index, signal::Waiter, store};
+use crate::{daemon, errors::*, index, signal::Waiter, store};
 
 pub struct App {
     store: store::DBStore,
